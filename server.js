1fbd38ab3a52c3782465df29618b77d82f7c6684
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-16 18:55:56 -0800

    broadcasts user count on connection and close

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
-const uuidv4 = require('uuid/v4');
+// const uuidv4 = require('uuid/v4');
 const PORT = 3001;
 
 // Create a new express server
@@ -14,22 +14,23 @@ const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-
-  // receiving messages from client
-  ws.onmessage = function(event) {
-    let incomingNote = JSON.parse(event.data);
-    let outgoingNote = {
-      note: incomingNote.note,
-    };
-
-    // broadcast note to all connected users
-    wss.clients.forEach(function each(client) {
-      let noteToSend = JSON.stringify(outgoingNote);
-      console.log('sending note');
-      client.send(noteToSend);
-    });
+  // set up user count on connection
+  let clientCountObject = {
+    type: 'userCount',
+    count: wss.clients.size,
   };
+  // loop through each connected client and send object to app
+  wss.clients.forEach(function each(client) {
+    client.send(JSON.stringify(clientCountObject));
+  });
+
   ws.on('close', () => {
     console.log('Client disconnected');
+    // reassign user count when client disconnects
+    clientCountObject.count = wss.clients.size;
+    // loop through clients and send new client count data
+    wss.clients.forEach(function each(client) {
+      client.send(JSON.stringify(clientCountObject));
+    });
   });
 });
